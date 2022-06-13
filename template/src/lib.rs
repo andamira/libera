322ac190ca83d00ce89ea83b761090cb0cdd8ad6
joseLuís <joseluis@andamira.net>
2022-06-13 18:40:50 +0200

    separate tests in new file

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -11,15 +11,5 @@
     clippy::pattern_type_mismatch
 )]
 #![forbid(unsafe_code)]
-
 //
-
 #![cfg_attr(not(feature = "std"), no_std)]
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq![1, true.into()];
-    }
-}
---
+++ template/src/tests.rs
@@ -0,0 +1,12 @@
+// ::
+//
+//!
+//
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq![1, true.into()];
+    }
+}
